50c4750b3a8b72c4faa6cf269cb4131450a19554
Tim Warren <kreas@pm.me>
2023-11-19 14:37:06 -0500

    WIP: Terms pages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -7,7 +8,11 @@ export default {
 			'sans': ['Montserrat', 'sans-serif'],
 		}
 	},
-	plugins: [require('@tailwindcss/typography'), require("daisyui")],
+	plugins: [
+		require('@tailwindcss/typography'),
+		require("daisyui"),
+		require('tailwindcss-textshadow') 
+	],
 	daisyui: {
 		themes: ['dark'],
 	}
